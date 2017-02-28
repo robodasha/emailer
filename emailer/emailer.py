c832ebb94fc9344904a12f30a721430d93bd8672
Drahomira Herrmannova <d.herrmannova@gmail.com>
2017-02-28 11:26:39 -0500

    Enabled empty CC address

--- emailer/emailer.py
+++ emailer/emailer.py
@@ -14,7 +14,8 @@ def send_email(from_address, copy_address, to_address, fname, subject, text):
     msg = MIMEText(text.replace('(%FIRST-NAME%)', fname))
     msg['Subject'] = subject
     msg['From'] = from_address
-    msg['Cc'] = copy_address
+    if copy_address is not None and len(copy_address):
+        msg['Cc'] = copy_address
     msg['To'] = to_address
     msg.add_header('reply-to', from_address)
 
